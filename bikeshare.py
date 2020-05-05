f1a944dbac6f87c5443b55e575f03274fd64331b
Gayathri <gayathrikanchet99@gmail.com>
2020-05-05 14:43:12 +0530

    Kept all comments at one place in get_filters function

--- bikeshare.py
+++ bikeshare.py
@@ -8,29 +8,23 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 cities=['chicago','new york city','washington']
 
 def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
-
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid input
+    # TO DO: get user input for month (all, january, february, ... , june)
+    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     city=input("Which city would you like to filter : ").lower()
     while(city not in cities):
         print("Please enter the valid city")
         city=input("Which city would you like to filter : ").lower()
 
-    # TO DO: get user input for month (all, january, february, ... , june)
+    
     months=['all','january', 'february', 'march', 'april', 'may', 'june']
     month=input("Which month would you like to filter : ").lower()
     while(month not in months):
         print("Please enter the valid month")
         city=input("Which month would you like to filter : ").lower()
 
-    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
+    
     days=['all','monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday','sunday']
     day=input("Which day of the week would you like to filter : ").lower()
     while(day not in days):
